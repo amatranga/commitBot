b45f9839784b951cce8d27d407b089a8f851f511
amatranga <amatranga892@gmail.com>
2017-11-10 15:52:29 -0800

    first commit

---
+++ CommitBot.psm1
@@ -0,0 +1,14 @@
+function CommitBot {
+  
+  Set-Location c:/users/alex/myapps/commitbot
+  Write-Output "Commit: $(date)" >> output.txt
+
+  git add output.txt
+  git commit -m "Made on: $(date)"
+  git push origin master
+
+  Set-Location ~
+}
+
+export-modulemember -function CommitBot
+  
